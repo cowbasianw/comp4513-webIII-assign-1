cdaab721dbe2185a0a2f7ca1f5eb9eb224ff0031
cowbasianw <77463732+cowbasianw@users.noreply.github.com>
2024-02-22 17:28:20 +0000

    completed

--- f4-server.js
+++ f4-server.js
@@ -259,20 +259,251 @@ app.get('/f4/races/circuit/:ref', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-app.get('/f4/drivers/name/:startsWith/limit/:limit', async (req, res) => {
-    const { startsWith, limit } = req.params;
-    const limitNumber = parseInt(limit);
+app.get('/f4/races/circuit/:ref/season/:startYear/:endYear', async (req, res) => {
+    try {
+        const { ref, startYear, endYear } = req.params;
+
+        // Retrieve race data for the specified circuit reference and range of years
+        const { data: circuitData, error: circuitError } = await supabase
+            .from('circuit')
+            .select() // Select all fields from both tables
+            .eq('circuitRef', ref)
+        if (circuitError) {
+            throw circuitError;
+        }
+
+        if (!circuitData || circuitData.length === 0) {
+            return res.status(404).json({ error: 'No race data found for the specified year' });
+        }
+
+        // Extract circuit IDs from circuit data
+        const circuitIds = circuitData.map(circuit => circuit.circuitId);
+        const { data: raceData, error: raceError } = await supabase
+            .from('races')
+            .select()
+            .eq('circuitId', circuitIds)
+            .gte('year', startYear) // Filter by start year
+            .lte('year', endYear) // Filter by end year
+            .order('year', { ascending: true }); // Order by year in ascending order
+
+        if (raceError) {
+            throw raceError;
+        }
+
+        if (!raceData || raceData.length === 0) {
+            return res.status(404).json({ error: 'No race data found for the specified year' });
+        }
+
+        res.json(raceData);
+    } catch (error) {
+        console.error('Error fetching race data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/f4/result/:raceId', async (req, res) => {
+    const { raceId } = req.params;
+    // Fetch drivers whose surname begins with the provided value, sorted by surname in ascending order, and limited to the provided number
+    try {
+        const { data, error } = await supabase
+            .from('result')
+            .select(`resultId, raceId (name, round, year, date), driverId(driverRef, code, forename, surname),
+                 constructorId (name, constructorRef, nationality), number, grid, position
+                , positionText, positionOrder, points, laps, time, milliseconds, fastestLap,
+                rank, fastestLapTime, fastestLapSpeed, statusId`)
+            .eq('raceId', raceId)
+            .order('grid', { ascending: true });
+
+        if (error) {
+            throw error;
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(404).json({ error: 'No data found for the specified raceId' });
+        }
+        res.send(data);
+    } catch (error) {
+        console.error('Error fetching race data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+});
+app.get('/f4/result/drivers/:driverRef', async (req, res) => {
+    try {
+        const { driverRef } = req.params;
+
+        // Retrieve driver data for the specified driverRef
+        const { data: driverData, error: driverError } = await supabase
+            .from('drivers')
+            .select()
+            .eq('driverRef', driverRef);
+
+        if (driverError) {
+            throw driverError;
+        }
+
+        if (!driverData || driverData.length === 0) {
+            return res.status(404).json({ error: 'No driver data found for the specified Ref' });
+        }
+
+        // Extract driverId from driver data
+        const driverId = driverData[0].driverId;
+
+        // Retrieve result data for the specified driverId
+        const { data: resultData, error: resultError } = await supabase
+            .from('result')
+            .select()
+            .eq('driverId', driverId);
+
+        if (resultError) {
+            throw resultError;
+        }
+
+        if (!resultData || resultData.length === 0) {
+            return res.status(404).json({ error: 'No result data found for the specified driver' });
+        }
+
+
+        res.json(resultData);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/f4/drivers/search/:startsWith', async (req, res) => {
+    const { startsWith } = req.params;
+
     // Fetch drivers whose surname begins with the provided value, sorted by surname in ascending order, and limited to the provided number
     const { data, error } = await supabase
         .from('drivers')
         .select('*')
         .ilike('surname', `${startsWith}%`) // Using ilike for case-insensitive matching
         .order('surname', { ascending: true })
-        .limit(limitNumber);
+
 
     res.send(data);
 
 });
+
+app.get('/f4/result/drivers/:driverRef/seasons/:start/:end', async (req, res) => {
+    try {
+        const { driverRef, start, end } = req.params;
+
+        // Retrieve driver data for the specified driverRef
+        const { data: driverData, error: driverError } = await supabase
+            .from('drivers')
+            .select()
+            .eq('driverRef', driverRef);
+
+        if (driverError) {
+            throw driverError;
+        }
+
+        if (!driverData || driverData.length === 0) {
+            return res.status(404).json({ error: 'No driver data found for the specified Ref' });
+        }
+
+        // Extract driverId from driver data
+        const driverId = driverData[0].driverId;
+
+
+        // Retrieve result data for the specified driverId within the specified range of years
+        const { data: resultData, error: resultError } = await supabase
+            .from('result')
+            .select('*, raceId(year)')
+            .eq('driverId', driverId);
+
+        if (resultError) {
+            throw resultError;
+        }
+
+        if (!resultData || resultData.length === 0) {
+
+            return res.status(404).json({ error: 'No result data found for the specified driver within the specified range of years' });
+        }
+        // Extract the years from the result data
+        const years = resultData.map(result => result.raceId.year);
+
+        // Filter the resultData based on the specified range of years and driverId
+        const filteredData = resultData.filter(result => {
+            const raceYear = result.raceId.year;
+            return raceYear >= start && raceYear <= end && result.driverId === driverId;
+        });
+        res.json(filteredData);
+
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/f4/qualifying/:raceId', async (req, res) => {
+    const { raceId } = req.params;
+    try {
+        const { data, error } = await supabase
+            .from('qualifying')
+            .select(`*, raceId (name, round, year, date), driverId(driverRef, code, forename, surname),
+                 constructorId (name, constructorRef, nationality)`)
+            .eq('raceId', raceId)
+            .order('position', { ascending: true });
+
+        if (error) {
+            throw error;
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(404).json({ error: 'No data found for the specified raceId' });
+        }
+        res.send(data);
+    } catch (error) {
+        console.error('Error fetching race data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+});
+app.get('/f4/standings/:raceId/drivers', async (req, res) => {
+    const { raceId } = req.params;
+    try {
+        const { data, error } = await supabase
+            .from('driversStandings')
+            .select(`*, driverId(driverRef, code, forename, surname)`)
+            .eq('raceId', raceId)
+            .order('position', { ascending: true });
+
+        if (error) {
+            throw error;
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(404).json({ error: 'No data found for the specified raceId' });
+        }
+        res.send(data);
+    } catch (error) {
+        console.error('Error fetching race data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/f4/standings/:raceId/constructors', async (req, res) => {
+    const { raceId } = req.params;
+    try {
+        const { data, error } = await supabase
+            .from('constructorStandings')
+            .select(`*, constructorId(constructorRef, name, nationality, url)`)
+            .eq('raceId', raceId)
+            .order('position', { ascending: true });
+
+        if (error) {
+            throw error;
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(404).json({ error: 'No data found for the specified raceId' });
+        }
+        res.send(data);
+    } catch (error) {
+        console.error('Error fetching constructor data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.listen(8080, () => {
     console.log('listening on port 8080');
@@ -282,10 +513,17 @@ app.listen(8080, () => {
     console.log('http://localhost:8080/f4/drivers');
     console.log('http://localhost:8080/f4/constructor');
     console.log('http://localhost:8080/f4/drivers/hamilton');
+    console.log('http://localhost:8080/f4/drivers/search/sch')
     console.log('http://localhost:8080/f4/constructor/mclaren');
     console.log('http://localhost:8080/f4/drivers/race/1106');
     console.log('http://localhost:8080/f4/races/season/2020');
     console.log('http://localhost:8080/f4/races/season/2022/4');
     console.log('http://localhost:8080/f4/races/circuit/monza');
-
+    console.log('http://localhost:8080/f4/races/circuit/monza/season/2020/2020');
+    console.log('http://localhost:8080/f4/result/1106');
+    console.log('http://localhost:8080/f4/result/drivers/max_verstappen');
+    console.log('http://localhost:8080/f4/result/drivers/sainz/seasons/2022/2022');
+    console.log('http://localhost:8080/f4/qualifying/1106');
+    console.log('http://localhost:8080/f4/standings/1106/drivers');
+    console.log('http://localhost:8080/f4/standings/1106/constructors');
 });
